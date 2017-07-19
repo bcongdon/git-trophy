967e52c1ed39cd30c599b3faff590c2e0a498c65
Benjamin Congdon <bencongdon96@gmail.com>
2017-07-19 06:03:23 -0700

    Cleanup blender.py into a python module

--- blender.py
+++ blender.py
@@ -2,8 +2,8 @@ import bpy
 import math
 from datetime import date
 from github_contributions import GithubUser
-
-YEAR = 2016
+import tempfile
+import os
 
 BAR_COLORS = [
     (0.933, 0.933, 0.933),
@@ -17,33 +17,34 @@ FONT_COLOR = (0.141, 0.161, 0.18)
 
 BASE_COLOR = (1, 1, 1)
 
-contributions = GithubUser('bcongdon').contributions(
-    end_date='{}-12-31'.format(YEAR)
-)
-
-counts = [x.count for x in contributions.days]
-levels = [x.level for x in contributions.days]
-
 
 def delete_all():
+    '''Deletes all objects in a scene
+    '''
     for ob in bpy.context.scene.objects:
         ob.select = True
     bpy.ops.object.delete()
 
 
 def create_bar_materials():
-    # Create materials for day bars
+    '''Creates materials for each of the BAR_COLORS
+    '''
     return [create_material(c, str(idx)) for idx, c in enumerate(BAR_COLORS)]
 
 
 def create_material(rgb, name):
+    '''Creates a material with a given color (rgb) and name (name)
+    '''
     mat = bpy.data.materials.new(name)
     mat.diffuse_color = rgb
     return mat
 
 
 def create_model_base(mat, num_weeks):
-    # Create base
+    '''Creates the rectangular solid
+
+        num_weeks is used to make sure that the base is of the correct length
+    '''
     width = num_weeks / 7.0
     bpy.ops.mesh.primitive_cube_add(location=(0, 0, 0))
     bpy.context.object.scale = (width, 1, 0.5)
@@ -52,11 +53,13 @@ def create_model_base(mat, num_weeks):
     base_obj.data.materials.append(mat)
 
 
-def create_user_label(mat):
-    # Create text label
+def create_user_label(mat, username, year):
+    '''Create a label in the side of the model base to display the user and
+        year
+    '''
     bpy.ops.object.text_add(location=(0, 0, 0))
     text_ob = bpy.context.object
-    text_ob.data.body = 'bcongdon / {}'.format(YEAR)
+    text_ob.data.body = '{} / {}'.format(username, year)
     text_ob.data.align_x = 'LEFT'
     text_ob.data.materials.append(mat)
     bpy.ops.transform.rotate(value=1.5708, axis=(1, 0, 0))
@@ -67,6 +70,8 @@ def create_user_label(mat):
 
 
 def coords_for_year_idx(idx):
+    '''Translates a day-of-year index into an XY coordinate for a day bar
+    '''
     x0 = -1/7
     y0 = -6/7
     week = math.floor(idx / 7)
@@ -76,12 +81,16 @@ def coords_for_year_idx(idx):
     return x, y
 
 
-def create_day_bars(mats):
-    # Create a bar for each day
+def create_day_bars(mats, counts, contributions, year):
+    ''' Creates a colored day bar for each day in the contributions list
+
+        Bar materials are taken from the 'mats' array
+    '''
+
     max_count = max(counts)
     max_z = 0.75
     for idx, day in enumerate(contributions.days):
-        if day.date < date(YEAR, 1, 1) or day.count == 0:
+        if day.date < date(year, 1, 1) or day.count == 0:
             continue
         x, y = coords_for_year_idx(idx)
         z = (float(day.count) / max_count) * max_z
@@ -93,20 +102,53 @@ def create_day_bars(mats):
 
 
 def join_all_objects():
-    # Join all objects into a mesh
+    ''' Joins all objects in the scene into a mesh
+    '''
     for ob in bpy.context.scene.objects:
         ob.select = True
     bpy.ops.object.join()
 
 
-delete_all()
-bar_mats = create_bar_materials()
-font_mat = create_material(FONT_COLOR, 'font')
-base_mat = create_material(BASE_COLOR, 'base')
-num_weeks = math.ceil(len(contributions.days) / 7)
-create_model_base(base_mat, num_weeks)
-create_user_label(font_mat)
-create_day_bars(bar_mats)
-join_all_objects()
-bpy.ops.export_scene.x3d(filepath='output.dae')
-bpy.ops.wm.quit_blender()
+def create_contribution_model(username, year):
+    '''Creates a model representing the user's contributions for the given year.
+
+        Returns the model as the string representation of an X3D file.
+
+        :param str username: The Github username of the requesting user
+        :param int year: The year of commits to be pulled
+
+        :returns: A string representation of an X3D model
+        :rtype: str
+    '''
+
+    contributions = GithubUser(username).contributions(
+        end_date='{}-12-31'.format(year)
+    )
+
+    counts = [x.count for x in contributions.days]
+
+    delete_all()
+    bar_mats = create_bar_materials()
+    font_mat = create_material(FONT_COLOR, 'font')
+    base_mat = create_material(BASE_COLOR, 'base')
+    num_weeks = math.ceil(len(contributions.days) / 7)
+    create_model_base(base_mat, num_weeks)
+    create_user_label(font_mat, username, year)
+    create_day_bars(bar_mats, counts, contributions, year)
+    join_all_objects()
+
+    # Get a temp file for saving the x3d data
+    fd, file_path = tempfile.mkstemp(suffix='.x3d')
+    os.close(fd)
+
+    # Save scene as x3d
+    bpy.ops.export_scene.x3d(filepath=file_path)
+
+    # Read back x3d data
+    with open(file_path, 'r') as f:
+        x3d_data = f.read()
+
+    # Remove temp file
+    os.remove(file_path)
+
+    return x3d_data
