ace6f9594ac467d6442e3009e629a55a57de540c
Benjamin Congdon <bencongdon96@gmail.com>
2017-08-08 08:19:04 -0700

    Work towards html templating

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 var webpack = require('webpack');
 var path = require('path');
+var fs = require('fs');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'public', 'dist');
 var APP_DIR = path.resolve(__dirname, 'src');
+
+var google_analytics = process.env.NODE_ENV === 'production' ? fs.readFileSync('src/google_analytics.html') : null;
 
 var config = {
   entry: APP_DIR + '/index.js',
@@ -22,6 +26,13 @@ var config = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      google_analytics: google_analytics,
+      filename: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   devServer: {
     contentBase: path.join(__dirname, "public"),
     publicPath: "/dist/",
