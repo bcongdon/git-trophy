c9e1461895f34b8afe5c5dd6a8905cfed93f304f
Benjamin Congdon <bencongdon96@gmail.com>
2017-08-11 05:53:56 -0700

    Fix Shapeways oauth redirect url

--- src/oauth.js
+++ src/oauth.js
@@ -1,7 +1,7 @@
 const config = {
   url: 'https://api.shapeways.com/oauth2/authorize',
   client: 'CddemRQQc1QYAkC8s2KNA0YPyl6GAYaVjWZDLIflz5wQTuESeG',
-  redirect: window.location.href,
+  redirect: window.location.protocol + "//" + window.location.host,
   scope: '',
   width: 800,
   height: 600
